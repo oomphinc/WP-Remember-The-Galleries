32e76b69b52f94fa82f1952628bb39079d60f2ab
Jonathan Cowher <jcowher@oomphinc.com>
2015-09-16 20:54:04 -0400

    Fix state when editing existing galleries
    Previously, when opening an existing gallery for editing, the user
    would be presented with the add new/upload media view and a few
    seconds later, the associated gallery images would load. There was
    no indication that anything was loading and the overall experience
    was kind of weird. By setting the state to "gallery-library", when
    the media window opens, a loading indicator is displayed until the
    images are fetched via ajax.

--- wp-remember-the-galleries/wp-remember-the-galleries.js
+++ wp-remember-the-galleries/wp-remember-the-galleries.js
@@ -471,6 +471,7 @@
 
 		wp.media.frame.open();
 		wp.media.frame.setIds(ids);
+		wp.media.frame.setState('gallery-library');
 	});
 
 	// Pop up media manager with edit gallery screen selected, for creating
